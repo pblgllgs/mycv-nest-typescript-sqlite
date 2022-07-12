9f1a796ba1b72be0396969630c09640351bce72a
pblgllgs <pbl.gllgs@gmail.com>
2022-07-11 21:32:34 -0400

    getEstimate

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, Patch, Param } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Patch, Param, Get, Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
@@ -7,6 +7,8 @@ import { User } from '../users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApprovedReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 
 @Controller('reports')
@@ -22,7 +24,14 @@ export class ReportsController {
     }
 
     @Patch('/:id')
+    @UseGuards(AdminGuard)
     approvedReport(@Param('id') id: string, @Body() body: ApprovedReportDto) {
         return this.reportsService.changeApproval(parseInt(id), body.approved);
     }
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto){
+        console.log(query);
+    }
+
 }
